b1bf0c1046c3fd3fa76d06a8815b9d1186f4ebfe
xislam <islammhasan@gmail.com>
2022-05-28 00:45:19 +0200

    added strings

---
+++ src/strings/index.js
@@ -0,0 +1,14 @@
+export const strings = {
+  defaultName: 'Default Name',
+  defaultPlaceholder: 'Default Placeholder',
+  defaultTitle: 'Default Title',
+  signupTxt: 'Sign up!',
+  namePlaceholder: 'Full Name',
+  emailPlaceholder: 'Email',
+  phonePlaceholder: 'Phone',
+  positionPlaceholder: 'Position',
+  addCandidate: 'Add Candidate',
+  required: '* Field required',
+  noCandidatesTxt: 'There are no candidates to show',
+  candidatesList: 'Candidates List'
+};
