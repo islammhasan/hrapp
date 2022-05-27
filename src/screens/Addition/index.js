bfc7fe9c4768d9674960722414a738ccb8b8a91f
xislam <islammhasan@gmail.com>
2022-05-28 00:47:41 +0200

    implemented addition screen

--- src/screens/Addition/index.js
+++ src/screens/Addition/index.js
@@ -1,15 +1,130 @@
-import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import { Container } from '../../components';
-import { styles } from './styles';
+import React, {useState} from 'react';
+import {Text} from 'react-native';
+import {useDispatch} from 'react-redux';
+import {Container, PrimaryButton, PrimaryInput} from '../../components';
+import {insertCandidate} from '../../redux/candidates';
+import {strings} from '../../strings';
+import {styles} from './styles';
 
 export const Addition = ({navigation}) => {
+  const [fullname, setFullname] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [position, setPosition] = useState('');
+  const [error, setError] = useState({
+    fullname: false,
+    email: false,
+    phone: false,
+    position: false,
+  });
+
+  const errorsInit = {
+    fullname: false,
+    email: false,
+    phone: false,
+    position: false,
+  };
+
+  const dispatch = useDispatch();
+
+  const candidate = {
+    fullname,
+    email,
+    phone,
+    position,
+  };
+
+  console.log(candidate);
+
+  const validation = {
+    fullname: fullname === '' || fullname.length < 2,
+    email: !String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      ),
+    phone: phone === '' || phone.length < 2,
+    position: position === '' || position.length < 2,
+  };
+
+  const addCandidate = () => {
+    switch (true) {
+      case validation.fullname:
+        setError({
+          ...error,
+          fullname: '* Full Name should have 2 characters at least',
+        });
+        break;
+      case validation.email:
+        setError({...error, email: '* Invalid email'});
+        break;
+      case validation.phone:
+        setError({...error, phone: '* Invalid phone number'});
+        break;
+      case validation.position:
+        setError({...error, position: '* Invalid input'});
+        break;
+      default: {
+        setError(errorsInit);
+        dispatch(insertCandidate(candidate));
+        navigation.navigate('CandidatesList');
+      }
+    }
+  };
+
   return (
-    <Container>
-      <Text>Addition</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('CandidatesList')}>
-        <Text>Candidates List</Text>
-      </TouchableOpacity>
+    <Container style={styles.container}>
+      <Text style={styles.title}>Candidate Info</Text>
+      <PrimaryInput
+        value={fullname}
+        onChangeText={fullname => {
+          setFullname(fullname);
+          setError({...error, fullname: false});
+        }}
+        error={error.fullname}
+        placeholder="Full Name"
+      />
+      <PrimaryInput
+        onChangeText={email => {
+          setEmail(email);
+          setError({...error, email: false});
+        }}
+        keyboardType={'email-address'}
+        error={error.email}
+        placeholder="Email"
+      />
+      <PrimaryInput
+        onChangeText={phone => {
+          setPhone(phone);
+          setError({...error, phone: false});
+        }}
+        keyboardType={'phone-pad'}
+        error={error.phone}
+        placeholder="Phone"
+      />
+      <PrimaryInput
+        onChangeText={position => {
+          setPosition(position);
+          setError({...error, position: false});
+        }}
+        error={error.position}
+        placeholder="Position"
+      />
+      <PrimaryButton
+        onPress={addCandidate}
+        title={strings.addCandidate}
+        style={styles.btnStyle}
+      />
+      <PrimaryButton
+        title={'Candidates List'}
+        onPress={() => navigation.navigate('CandidatesList')}
+        style={styles.secondaryBtnStyle}
+        titleStyle={styles.secondaryBtnTitle}
+      />
     </Container>
   );
 };
+
+
+// primary button and input => just input and button
+//
--- src/screens/Addition/styles.js
+++ src/screens/Addition/styles.js
@@ -1,5 +1,27 @@
-import { ScaledSheet } from 'react-native-size-matters';
+import {ScaledSheet} from 'react-native-size-matters';
+import {colors} from '../../assets/colors';
 
 export const styles = ScaledSheet.create({
-    
-})
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: '20@ms',
+    color: colors.blue,
+    marginBottom: '20@vs',
+    textTransform: 'uppercase',
+    fontWeight: '500',
+  },
+  btnStyle: {
+    marginTop: '50@vs',
+  },
+  secondaryBtnStyle: {
+    backgroundColor: null,
+    marginTop: '10@vs',
+  },
+  secondaryBtnTitle: {
+    color: colors.blue,
+    fontSize: 16,
+  },
+});
