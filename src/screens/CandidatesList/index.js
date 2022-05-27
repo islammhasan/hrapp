4d84014ec33b0e4854941cd1599393560f9add24
xislam <islammhasan@gmail.com>
2022-05-28 00:47:52 +0200

    implemented candidates list screen

--- src/screens/CandidatesList/index.js
+++ src/screens/CandidatesList/index.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import { Container } from '../../components';
-import { styles } from './styles';
+import {View, Text, FlatList, Alert} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {Container, PrimaryButton, ListItem} from '../../components';
+import {removeCandidate} from '../../redux/candidates';
+import {strings} from '../../strings';
+import {styles} from './styles';
 
 export const CandidatesList = ({navigation}) => {
+  const candidates = useSelector(state => state.candidates.data);
+  const dispatch = useDispatch();
+
+  const deleteCandidate = item => {
+    Alert.alert(
+      'Warning',
+      `You are about to delete "${item.fullname}" info, this action can not be undone! `,
+      [
+        {
+          text: 'CANCEL',
+          style: 'cancel',
+        },
+        {
+          text: 'DELETE',
+          onPress: () => dispatch(removeCandidate(item.id)),
+          style: 'destructive',
+        },
+      ],
+    );
+  };
+
+  const renderItem = ({item}) => {
+    return <ListItem removeItem={() => deleteCandidate(item)} item={item} />;
+  };
+
+  const separator = () => {
+    return <View style={styles.separator} />;
+  };
+
+  const noCandidates = candidates.length > 0;
+
   return (
-    <Container>
-      <Text>Candidates List</Text>
-      <TouchableOpacity onPress={() => navigation.navigate('Addition')}>
-        <Text>Addition</Text>
-      </TouchableOpacity>
+    <Container style={styles.container}>
+      <Text style={styles.title}>{strings.candidatesList}</Text>
+      {noCandidates ? (
+        <>
+          <FlatList
+            data={candidates}
+            contentContainerStyle={styles.listContainerStyle}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={item => item.id.toString()}
+            renderItem={renderItem}
+            ItemSeparatorComponent={separator}
+          />
+          <PrimaryButton
+            title="Add Candidate"
+            style={styles.btnStyle}
+            onPress={() => navigation.navigate('Addition')}
+          />
+        </>
+      ) : (
+        <View style={styles.secondaryContainer}>
+          <Text style={styles.noCandidatesText}>{strings.noCandidatesTxt}</Text>
+          <PrimaryButton
+            title="Add Candidate"
+            style={styles.btnStyle}
+            onPress={() => navigation.navigate('Addition')}
+          />
+        </View>
+      )}
     </Container>
   );
 };
--- src/screens/CandidatesList/styles.js
+++ src/screens/CandidatesList/styles.js
@@ -1,5 +1,37 @@
-import { ScaledSheet } from 'react-native-size-matters';
+import {ScaledSheet} from 'react-native-size-matters';
+import {colors} from '../../assets/colors';
 
 export const styles = ScaledSheet.create({
-    
-})
+  container: {
+    backgroundColor: colors.offWhite,
+  },
+  title: {
+    fontSize: '20@ms',
+    color: colors.blue,
+    marginVertical: '20@vs',
+    textTransform: 'uppercase',
+    fontWeight: '500',
+    marginStart: '20@s',
+  },
+  listContainerStyle: {
+    paddingHorizontal: '20@s',
+    paddingVertical: '10@vs',
+  },
+  btnStyle: {
+    alignSelf: 'center',
+  },
+  noCandidatesText: {
+    color: colors.darkgray,
+    fontSize: '16@ms',
+    textAlign: 'center',
+    marginBottom: '20@vs',
+  },
+  secondaryContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  separator: {
+    height: '15@vs',
+  },
+});
